ad0628d742c4efe86638f88156508f28f7f087fd
DingGGu <ggu@dunamu.com>
2024-02-29 16:39:36 +0900

    NLB AccessLog
    Signed-off-by: DingGGu <ggu@dunamu.com>

--- main.tf
+++ main.tf
@@ -3,22 +3,22 @@ resource "aws_glue_catalog_database" "lb_access_log" {
 }
 
 resource "aws_glue_catalog_table" "alb_logs" {
-  name = "alb_logs"
-  owner = "hadoop"
+  name          = "alb_logs"
+  owner         = "hadoop"
   database_name = aws_glue_catalog_database.lb_access_log.name
 
   table_type = "EXTERNAL_TABLE"
 
   parameters = {
-    "has_encrypted_data" = "false"
-    "EXTERNAL" = "TRUE"
-    "projection.enabled" = "true"
-    "projection.date_day.format" = "yyyy/MM/dd"
-    "projection.date_day.interval" = "1"
+    "has_encrypted_data"                = "false"
+    "EXTERNAL"                          = "TRUE"
+    "projection.enabled"                = "true"
+    "projection.date_day.format"        = "yyyy/MM/dd"
+    "projection.date_day.interval"      = "1"
     "projection.date_day.interval.unit" = "DAYS"
-    "projection.date_day.range" = var.partition_projection_range
-    "projection.date_day.type" = "date"
-    "storage.location.template" = "s3://${var.s3_bucket_name}/AWSLogs/${data.aws_caller_identity.current.account_id}/elasticloadbalancing/${data.aws_region.current.name}/$${date_day}"
+    "projection.date_day.range"         = var.partition_projection_range
+    "projection.date_day.type"          = "date"
+    "storage.location.template"         = "s3://${var.s3_bucket_name}/AWSLogs/${data.aws_caller_identity.current.account_id}/elasticloadbalancing/${data.aws_region.current.name}/$${date_day}"
   }
 
   retention = 0
@@ -29,12 +29,12 @@ resource "aws_glue_catalog_table" "alb_logs" {
   }
 
   storage_descriptor {
-    bucket_columns = []
-    compressed = false
-    input_format = "org.apache.hadoop.mapred.TextInputFormat"
-    location = "s3://${var.s3_bucket_name}/AWSLogs/${data.aws_caller_identity.current.account_id}/elasticloadbalancing/${data.aws_region.current.name}"
-    number_of_buckets = -1
-    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+    bucket_columns            = []
+    compressed                = false
+    input_format              = "org.apache.hadoop.mapred.TextInputFormat"
+    location                  = "s3://${var.s3_bucket_name}/AWSLogs/${data.aws_caller_identity.current.account_id}/elasticloadbalancing/${data.aws_region.current.name}"
+    number_of_buckets         = -1
+    output_format             = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
     stored_as_sub_directories = false
 
     columns {
@@ -172,7 +172,149 @@ resource "aws_glue_catalog_table" "alb_logs" {
 
     ser_de_info {
       parameters = {
-        "input.regex" = "([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*):([0-9]*) ([^ ]*)[:-]([0-9]*) ([-.0-9]*) ([-.0-9]*) ([-.0-9]*) (|[-0-9]*) (-|[-0-9]*) ([-0-9]*) ([-0-9]*) \"([^ ]*) ([^ ]*) (- |[^ ]*)\" \"([^\"]*)\" ([A-Z0-9-]+) ([A-Za-z0-9.-]*) ([^ ]*) \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" ([-.0-9]*) ([^ ]*) \"([^\"]*)\" \"([^\"]*)\" \"([^ ]*)\" \"([^\\s]+?)\" \"([^\\s]+)\" \"([^ ]*)\" \"([^ ]*)\""
+        "input.regex"          = "([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*):([0-9]*) ([^ ]*)[:-]([0-9]*) ([-.0-9]*) ([-.0-9]*) ([-.0-9]*) (|[-0-9]*) (-|[-0-9]*) ([-0-9]*) ([-0-9]*) \"([^ ]*) ([^ ]*) (- |[^ ]*)\" \"([^\"]*)\" ([A-Z0-9-]+) ([A-Za-z0-9.-]*) ([^ ]*) \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" ([-.0-9]*) ([^ ]*) \"([^\"]*)\" \"([^\"]*)\" \"([^ ]*)\" \"([^\\s]+?)\" \"([^\\s]+)\" \"([^ ]*)\" \"([^ ]*)\""
+        "serialization.format" = "1"
+      }
+      serialization_library = "org.apache.hadoop.hive.serde2.RegexSerDe"
+    }
+  }
+}
+
+resource "aws_glue_catalog_table" "nlb_logs" {
+  name          = "nlb_logs"
+  owner         = "hadoop"
+  database_name = aws_glue_catalog_database.lb_access_log.name
+
+  table_type = "EXTERNAL_TABLE"
+
+  parameters = {
+    "has_encrypted_data"                = "false"
+    "EXTERNAL"                          = "TRUE"
+    "projection.enabled"                = "true"
+    "projection.date_day.format"        = "yyyy/MM/dd"
+    "projection.date_day.interval"      = "1"
+    "projection.date_day.interval.unit" = "DAYS"
+    "projection.date_day.range"         = var.partition_projection_range
+    "projection.date_day.type"          = "date"
+    "storage.location.template"         = "s3://${var.s3_bucket_name}/AWSLogs/${data.aws_caller_identity.current.account_id}/elasticloadbalancing/${data.aws_region.current.name}/$${date_day}"
+  }
+
+  retention = 0
+
+  partition_keys {
+    name = "date_day"
+    type = "string"
+  }
+
+  storage_descriptor {
+    bucket_columns            = []
+    compressed                = false
+    input_format              = "org.apache.hadoop.mapred.TextInputFormat"
+    location                  = "s3://${var.s3_bucket_name}/AWSLogs/${data.aws_caller_identity.current.account_id}/elasticloadbalancing/${data.aws_region.current.name}"
+    number_of_buckets         = -1
+    output_format             = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+    stored_as_sub_directories = false
+
+    columns {
+      name = "type"
+      type = "string"
+    }
+    columns {
+      name = "version"
+      type = "string"
+    }
+    columns {
+      name = "time"
+      type = "string"
+    }
+    columns {
+      name = "elb"
+      type = "string"
+    }
+    columns {
+      name = "listener_id"
+      type = "string"
+    }
+    columns {
+      name = "client_ip"
+      type = "string"
+    }
+    columns {
+      name = "client_port"
+      type = "int"
+    }
+    columns {
+      name = "target_ip"
+      type = "string"
+    }
+    columns {
+      name = "target_port"
+      type = "int"
+    }
+    columns {
+      name = "tcp_connection_time_ms"
+      type = "double"
+    }
+    columns {
+      name = "tls_handshake_time_ms"
+      type = "double"
+    }
+    columns {
+      name = "received_bytes"
+      type = "bigint"
+    }
+    columns {
+      name = "sent_bytes"
+      type = "bigint"
+    }
+    columns {
+      name = "incoming_tls_alert"
+      type = "int"
+    }
+    columns {
+      name = "cert_arn"
+      type = "string"
+    }
+    columns {
+      name = "certificate_serial"
+      type = "string"
+    }
+    columns {
+      name = "tls_cipher_suite"
+      type = "string"
+    }
+    columns {
+      name = "tls_protocol_version"
+      type = "string"
+    }
+    columns {
+      name = "tls_named_group"
+      type = "string"
+    }
+    columns {
+      name = "domain_name"
+      type = "string"
+    }
+    columns {
+      name = "alpn_fe_protocol"
+      type = "string"
+    }
+    columns {
+      name = "alpn_be_protocol"
+      type = "string"
+    }
+    columns {
+      name = "alpn_client_preference_list"
+      type = "string"
+    }
+    columns {
+      name = "tls_connection_creation_time"
+      type = "string"
+    }
+
+    ser_de_info {
+      parameters = {
+        "input.regex"          = "([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*):([0-9]*) ([^ ]*):([0-9]*) ([-.0-9]*) ([-.0-9]*) ([-0-9]*) ([-0-9]*) ([-0-9]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*)$"
         "serialization.format" = "1"
       }
       serialization_library = "org.apache.hadoop.hive.serde2.RegexSerDe"
